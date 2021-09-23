bd4116170eec0b956fd1e83ea55779aa5e81db68
Astirian <burgercat@gmail.com>
2021-09-23 09:10:54 +0800

    Added state manager, main menu dismissal and moved character controller script to appropriate scripts folder.

---
+++ Assets/Scripts/Managers/StateManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Doozy.Engine.UI;
+
+public class StateManager : MonoBehaviour
+{
+    public static StateManager core;
+    public bool gameStarted = false;
+    public UIView mainMenu;
+
+    void Awake()
+    {
+        if (core == null) { core = this; }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!gameStarted) 
+            { 
+                gameStarted = true;
+                HideMainMenu();
+            }
+        }
+    }
+
+    public void HideMainMenu()
+    {
+        mainMenu.transform.GetComponent<Image>().enabled = false;
+        mainMenu.Hide();
+    }
+}
